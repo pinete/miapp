011e4d6cb46d1f4a86bb15bb1116a639a6574ab0
Pinete <luis.domech@gmail.com>
2025-09-17 21:45:20 +0200

    otro

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,13 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+        },
+    },
+
 });
