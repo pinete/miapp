3e29463a6cfd73c71c99297d7bf04fd8f172fa0b
Luis Domech <100071106+pinete@users.noreply.github.com>
2025-09-17 10:58:53 +0200

    merge pendiente

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,14 @@ use App\DataTables\ClientesDataTable;
 // Rutas básicas de Laravel. Pagina de bienvenida. Activa la vista resources/views/welcome.blade.php
 Route::get('/', function () {
     return view('welcome');
-
 });
 
-//Route::get('/{entidad}', [EntidadController::class, 'index']); // Listar registros de la entidad
 Route::get('/{entidad}/data', [EntidadController::class, 'getRegistrosEntidad']); // Obtener datos vía AJAX para DataTables
 Route::get('/{entidad}/create', [EntidadController::class, 'create']); // Mostrar formulario de creación
-//Route::post('/{entidad}', [EntidadController::class, 'store']); // Guardar nuevo registro
 Route::post('/{entidad}/store', [EntidadController::class, 'store'])->name('entidad.store'); // Guardar nuevo registro
 Route::get('/{entidad}/{id}/edit', [EntidadController::class, 'edit']); // Mostrar formulario de edición
 Route::put('/{entidad}/{id}', [EntidadController::class, 'update']); // Actualizar registro
-//Route::delete('/{entidad}/{id}', [EntidadController::class, 'destroy']); // Eliminar registro
 Route::delete('/{entidad}/{id}', [EntidadController::class, 'destroy'])->name('entidad.destroy');
-//Route::get('/{entidad}/{id}/json', [EntidadController::class, 'showJson']); // Obtener datos de un registro específico en formato JSON
 Route::get('/{entidad}/{id}/json', [EntidadController::class, 'showJson'])->name('entidad.json'); // Obtener datos de un registro específico en formato JSON
 Route::get('/{entidad}/{id}', [EntidadController::class, 'show']); // Mostrar detalles de un registro específico
 Route::post('/{entidad}/store', [EntidadController::class, 'store'])->name('entidad.store');
