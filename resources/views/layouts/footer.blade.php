6fcfbaeb0ae27e7e9274408a6be6b329d7ac2a07
Pinete <luis.domech@gmail.com>
2025-09-29 11:16:42 +0200

    Ahora si funciona el colReorder - Faltaba cargar los colReorder js y ccs

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -12,6 +12,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/colreorder/1.6.2/js/dataTables.colReorder.min.js"></script>
 
     @yield('scripts')
 </body>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,6 +14,8 @@
     <!-- CSS de DataTables -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/colreorder/1.6.2/css/colReorder.bootstrap5.min.css">
+
 
 </head>
 <body>
